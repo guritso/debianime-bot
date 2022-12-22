316048623a32191d79ac34727dd4356fd8a544ab
gurizenit <gurizenit@gmail.com>
2022-12-21 23:04:28 -0300

    update: constructor in ping.js

--- Commands/general/ping.js
+++ Commands/general/ping.js
@@ -1,10 +1,18 @@
 export default class {
-  constructor() {}
-  async execute(interaction) {
-    const lat = Date.now() - inter.createdTimestamp;
+  constructor() {
+    this.data = {
+      name: "ping",
+      description: "send de ping",
+    };
+  }
+  async execute(interaction, client) {
+    const lat = Date.now() - interaction.createdTimestamp;
     const api = Math.round(client.ws.ping);
     const msg1 = "`latency:`" + `**${lat}ms**\n`;
-    const msg2 = "`api`   :" + `**${api}ms**`;
-    interaction.reply({ content: msg1 + msg2 });
+    const msg2 = "`api   :`" + `**${api}ms**`;
+    interaction.reply({
+      content: msg1 + msg2,
+      ephemeral: true
+  })
   }
 }
