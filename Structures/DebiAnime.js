0478a7fe68aaa84581e225dca1dcd9ffe44a0553
gurizenit <gurizenit@gmail.com>
2022-12-21 23:14:03 -0300

    add: logs.js, update: Events.js / DebiAnime.js

--- Structures/DebiAnime.js
+++ Structures/DebiAnime.js
@@ -1,19 +1,30 @@
-import { Client } from "discord.js";
-import { clientOptions } from "./clientOptions.js";
+import { clientOptions } from "../Configs/clientOptions.js";
+import { Client, Collection } from "discord.js";
+import Logs from "../Utils/Logs.js";
+import Handler from "./Handler.js";
 import Events from "./Events.js";
+import Slash from "./Slash.js";
 
 export default class DebiAnime extends Client {
   constructor(options = {}) {
     super(clientOptions);
+
+    this.commands = new Collection();
+    this.logs = new Logs();
     this.loader(options);
   }
 
   loader(options) {
     this.token = options.bot.TOKEN;
+    this.guild = options.guild;
+    this.bot = options.bot;
   }
 
   async start() {
-    await new Events(this).execute();
+    await new Events(this).execute(this.logs);
+    await new Slash(this).execute(this.logs);
+    await new Handler(this).execute(this.logs);
     await super.login();
+    await this.logs.start();
   }
 }
