7db574f0e25972b5db38ab4b0257ee5bf69af987
gurizenit <gurizenit@gmail.com>
2022-12-22 22:22:46 -0300

    update: embed in ping.js

--- src/Commands/general/ping.js
+++ src/Commands/general/ping.js
@@ -2,17 +2,25 @@ export default class {
   constructor() {
     this.data = {
       name: "ping",
-      description: "send de ping",
+      description: "send the ping",
     };
   }
   async execute(interaction, client) {
-    const lat = Date.now() - interaction.createdTimestamp;
+    const bot = Date.now() - interaction.createdTimestamp;
     const api = Math.round(client.ws.ping);
-    const msg1 = "`latency:`" + `**${lat}ms**\n`;
-    const msg2 = "`api   :`" + `**${api}ms**`;
+
+    const embed = {
+      title: "PING",
+      fields: [
+        { name: "BOT:", value: `${bot}ms`, inline: true },
+        { name: "API:", value: `${api}ms`, inline: true },
+      ],
+      color: 0x000000,
+    };
+
     interaction.reply({
-      content: msg1 + msg2,
-      ephemeral: true
-  })
+      embeds: [embed],
+      ephemeral: true,
+    });
   }
 }
