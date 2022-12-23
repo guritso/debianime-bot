895a9e07ebbc04e70a36b72f3c40c606794ec620
gurizenit <gurizenit@gmail.com>
2022-12-23 13:37:26 -0300

    add: /Configs/colors.js

---
+++ src/Configs/colors.js
@@ -0,0 +1,18 @@
+export default {
+  hex: {
+    primary: "#FF0044",
+    secondary: "#000000",
+    yellow: "#ffee09",
+    red: "#f32323",
+    green: "#00ff23",
+    blue: "#0060ff",
+  },
+  int: {
+    primary: 0xff0044,
+    secondary: 0x000000,
+    yellow: 0xffee09,
+    red: 0xf32323,
+    green: 0x00ff23,
+    blue: 0x0060ff
+  },
+};
