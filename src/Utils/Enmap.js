ecad550ec116bfce2accacce877f44da08df1ac1
gurizenit <gurizenit@gmail.com>
2022-12-26 21:24:42 -0300

    update: bug not counting fix in Enmap.js

--- src/Utils/Enmap.js
+++ src/Utils/Enmap.js
@@ -4,25 +4,28 @@ export default class Enmap {
   constructor(path) {
     this.PATH = process.cwd() + path;
     this.folders = readdirSync(this.PATH);
-    this.count = 0;
   }
-  async execute (map) {
+  async execute(map) {
     await new Promise((resolve) => {
       const PATH = this.PATH;
 
       this.folders.forEach((dir) => {
-        this.files = readdirSync(`${PATH}/${dir}`).filter((name) =>
+        const files = readdirSync(`${PATH}/${dir}`).filter((name) =>
           name.endsWith(".js")
         );
+        const length = files.length;
+        let count = 0;
 
-        this.files.forEach(async (cmd) => {
+        files.forEach(async (cmd) => {
           const Load = await import(`${PATH}/${dir}/${cmd}`);
           const load = await new Load.default();
 
           map.set(load.data.name, load);
 
-          this.count = this.count + 1;
-          if (this.count == this.files.length) {
+          console.log(`• ${load.data.name}`);
+
+          count = count + 1;
+          if (count == length) {
             resolve();
           }
         });
