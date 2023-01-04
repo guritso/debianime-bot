cf98006d327a593f146c7435852f4a78349c13ce
gurizenit <gurizenit@gmail.com>
2023-01-03 21:16:55 -0300

    update: permissions in messageCreate.js added permissions checker

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,22 +1,51 @@
+import Permission from "../Utils/Permission.js";
+
 export default class {
   constructor() {
     this.once = false;
   }
   async execute(message) {
-    if (message.author.bot) return;
+    const { client, author, content, channel } = message;
+    const { bot, messageCommands } = client;
 
-    const client = message.client;
-    const prefix = client.bot.prefix;
+    if (author.bot) return;
 
-    if (!message.content.toLowerCase().startsWith(prefix)) return;
+    if (!content.toLowerCase().startsWith(bot.prefix)) return;
 
-    const args = message.content.trim().slice(prefix.length).split(/ +/g);
-
-    const messageCommands = client.messageCommands;
+    const args = content.trim().slice(bot.prefix.length).split(/ +/g);
     const commandName = args.shift().toLowerCase();
     const command = messageCommands.get(commandName);
 
+    console.log(commandName, command);
     if (!command) return;
+
+    const { permissionsBot, permissionsUser } = command.data;
+
+    const permission = new Permission()
+      .setNeeded(permissionsBot)
+      .setMemberId(client.user.id)
+      .setChannel(channel);
+    const missingClient = await permission.getMissing();
+
+    let embed = {
+      title: "Missing Permissions",
+      color: client.color.int.red,
+    };
+
+    if (missingClient) {
+      embed.description = missingClient.join("\n");
+      embed.footer = { text: `For ${client.user.tag}` };
+      return message.member.send({ embeds: [embed] });
+    }
+
+    permission.setMemberId(author.id).setNeeded(permissionsUser);
+    const missingAuthor = await permission.getMissing();
+
+    if (missingAuthor) {
+      embed.description = missingAuthor.join("\n");
+      embed.footer = { text: `For ${author.tag}` };
+      return message.member.send({ embeds: [embed] });
+    }
 
     try {
       command.execute(message, args, client);
