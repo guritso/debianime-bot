308b123d6864b63626866c62cfe24364ee0e8e2d
gurizenit <gurizenit@gmail.com>
2023-01-12 01:05:33 +0000

    update: set improvement in Database.js

--- src/Collections/Database.js
+++ src/Collections/Database.js
@@ -13,7 +13,16 @@ export default class Database extends Client {
     for (let key of data) {
       const value = await super.get(key);
       this.cache.set(key, value);
+      console.log("  •", key);
     }
-    console.log(this.cache)
+
+    this.client.database = this;
+  }
+  async set(key, value) {
+    const data = this.cache.get(key);
+    const join = { ...data, ...value };
+
+    await super.set(key, join);
+    this.cache.set(key, join);
   }
 }
