345e4d4d6392cae8ffa9635fb6df188d720a9ec8
gurizenit <gurizenit@gmail.com>
2023-01-17 21:40:24 -0300

    add: general slash command levels.js

---
+++ src/Commands/Slash/General/levels.js
@@ -0,0 +1,47 @@
+export default class {
+  constructor() {
+    this.data = {
+      type: 1,
+      name: "levels",
+      description: "Show the users level ranking",
+    };
+  }
+  execute(interaction) {
+    const { client, guild, user } = interaction;
+    const { database } = client;
+
+    if (!database.cache.get(guild.id)) {
+      return interaction.reply("> There's no raking!");
+    }
+
+    const ranking = [];
+    const author = {};
+
+    const levels = database.cache.get(guild.id).levels;
+
+    levels.sort((a, b) => b.level - a.level);
+    levels.forEach((member, index) => {
+      if (index + 1 >= 10) return;
+      if (member.level == 0) return;
+
+      ranking.push(`#${index + 1} • ${member.tag} • LVL: **${member.level}**`);
+
+      if (user.id == member.id) {
+        author.level = member.level;
+        author.position = index + 1;
+      }
+    });
+
+    const embed = {
+      title: `Levels on ${guild.name}`,
+      description: ranking.join("\n"),
+      color: client.color.int.primary,
+      footer: {
+        text: `#${author.position} • ${user.tag} • LVL: ${author.level}`,
+        icon_url: user.avatarURL(),
+      },
+    };
+
+    interaction.reply({ embeds: [embed] });
+  }
+}
