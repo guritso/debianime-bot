3c3b5dc8f2cad5587bac16a7755269072e18121a
gurizenit <gurizenit@gmail.com>
2023-01-18 02:44:00 -0300

    update: fix crash in prune.js it crash if message is old > 14 days

--- src/Commands/Slash/Manager/prune.js
+++ src/Commands/Slash/Manager/prune.js
@@ -21,19 +21,23 @@ export default class {
     this.strings = {
       invalid: "> Invalid ammount! `1 < ammount < 100`",
       deleted: "messages deleted!",
+      error: "> I can't delete messages from 14 days ago!",
     };
   }
   async execute(interaction, client) {
     await interaction.deferReply({ ephemeral: true });
-    const { invalid, missing, deleted } = this.strings;
+    const { invalid, error, deleted } = this.strings;
     const ammount = interaction.options.getInteger("ammount");
 
     if (2 > ammount || ammount > 100) {
       return interaction.editReply(invalid);
     }
 
-    const messages = await interaction.channel.bulkDelete(ammount);
+    const messages = await interaction.channel.bulkDelete(ammount).catch(() => {});
 
+    if (!messages) {
+      return interaction.editReply(error);
+    }
     interaction.editReply({
       content: `> **${messages.size}** ${deleted}`,
     });
