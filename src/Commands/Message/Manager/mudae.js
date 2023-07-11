94a3602f9ba1b644eef575a7184cfed1cf64499d
gurizenit <gurizenit@gmail.com>
2023-07-11 01:06:13 -0300

    update: mudae command aliases

--- src/Commands/Message/Manager/mudae.js
+++ src/Commands/Message/Manager/mudae.js
@@ -3,7 +3,7 @@ export default class {
     this.data = {
       name: "mudae",
       description: "Add react to Mudae bot rolls",
-      aliases: ["react"],
+      aliases: ["react, mu, rolls"],
       permissionsBot: ["AddReactions"],
       permissionsUser: ["ManageMessages"],
     };
