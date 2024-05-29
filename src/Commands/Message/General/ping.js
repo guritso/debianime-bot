28349b22093fdd02d515f8126250a2e223370b49
GuriZenit <gurizenit@gmail.com>
2024-05-29 16:41:35 +0000

    tab size

--- src/Commands/Message/General/ping.js
+++ src/Commands/Message/General/ping.js
@@ -1,28 +1,28 @@
 export default class {
-    constructor() {
-        this.data = {
-            name: "ping",
-            description: "Show api ping and bot ping.",
-            aliases: ["ms"],
-            permissionsBot: ["EmbedLinks", "SendMessages"],
-        }
+  constructor() {
+    this.data = {
+      name: "ping",
+      description: "Show api ping and bot ping.",
+      aliases: ["ms"],
+      permissionsBot: ["EmbedLinks", "SendMessages"],
     }
-    async execute (message, args, client) {
-        const bot = Date.now() - message.createdTimestamp;
-        const api = Math.round(client.ws.ping);
-    
-        const embed = {
-          title: "PING",
-          fields: [
-            { name: "BOT:", value: `${bot}ms`, inline: true },
-            { name: "API:", value: `${api}ms`, inline: true },
-          ],
-          color: client.config.color.int.primary,
-        };
-    
-        message.reply({
-          embeds: [embed],
-          ephemeral: true,
-        });
-    }
+  }
+  async execute(message, args, client) {
+    const bot = Date.now() - message.createdTimestamp;
+    const api = Math.round(client.ws.ping);
+
+    const embed = {
+      title: "PING",
+      fields: [
+        { name: "BOT:", value: `${bot}ms`, inline: true },
+        { name: "API:", value: `${api}ms`, inline: true },
+      ],
+      color: client.config.color.int.primary,
+    };
+
+    message.reply({
+      embeds: [embed],
+      ephemeral: true,
+    });
+  }
 }
