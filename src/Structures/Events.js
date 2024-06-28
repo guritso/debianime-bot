9dea39f41b2cacdf72a271ecb1216ccd8f4e81da
GuriZenit <gurizenit@gmail.com>
2024-06-28 06:06:06 +0000

    refactor: optimize event loading and execution

--- src/Structures/Events.js
+++ src/Structures/Events.js
@@ -4,32 +4,30 @@ export default class Events {
   constructor(client) {
     this.client = client;
   }
+
   async execute() {
     const PATH = process.cwd() + "/src/Events";
     const files = readdirSync(PATH);
     const client = this.client;
 
-    return new Promise(async (resolve) => {
-      for (let file of files) {
-        const eventName = file.replace(".js", "");
-        const Event = await import(`${PATH}/${file}`);
-        const event = new Event.default();
+    const promises = files.map(async (file) => {
+      const eventName = file.replace(".js", "");
+      const Event = await import(`${PATH}/${file}`);
+      const event = new Event.default();
 
-        if (event.once) {
-          client.once(eventName, (...args) => {
-            event.execute(...args);
-          });
-        } else {
-          client.on(eventName, (...args) => {
-            event.execute(...args);
-          });
-        }
-
-        console.log(`  • ${eventName}`);
-        if (files.indexOf(file) == files.length - 1) {
-          resolve();
-        }
+      if (event.once) {
+        client.once(eventName, (...args) => {
+          event.execute(...args);
+        });
+      } else {
+        client.on(eventName, (...args) => {
+          event.execute(...args);
+        });
       }
+
+      console.log(`  • ${eventName}`);
     });
+
+    await Promise.all(promises);
   }
 }
