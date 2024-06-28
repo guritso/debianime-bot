6c92b39ca33c81f6350262ba480516c424f04cee
GuriZenit <gurizenit@gmail.com>
2024-06-28 05:10:01 +0000

    update LevelSystem code

--- src/Structures/LevelSystem.js
+++ src/Structures/LevelSystem.js
@@ -8,58 +8,57 @@ export default class LevelSystem {
     };
     this.message = message;
   }
+
   async execute(database) {
     const { guild, client } = this.message;
     const { id, name } = this.body;
-    // get the user from the database
     const { members, channels } = database.cache.get(guild.id);
     const userData = members.find((user) => user.id == id);
-    // check if user exists, add if not
+
     if (!userData) {
       members.push(this.body);
       return database.set(guild.id, { members });
     }
-    // add experience
-    const experience = addExperience(userData);
-    // add level if experience reach the target
-    if (experience >= getTarget(userData)) {
-      const userLevel = addLevel(userData);
 
-      const embed = {
-        title: "Level Up",
-        color: client.config.color.int.primary,
-        description: `${name} as reached level **${userLevel}**!`,
-      };
-      // get the channel using the id from database
-      const channel = guild.channels.cache.get(channels.ranking);
-      // checks if have one
-      if (channel) {
-        // get the permissions from the channel
-        const permissions = channel.permissionsFor(client.user.id);
+    const experience = this.addExperience(userData);
 
-        if (permissions.has(["EmbedLinks", "SendMessages"])) {
-          channel.send({ content: `<@${id}>`, embeds: [embed] });
-        }
-      }
-      
-      
+    if (experience >= this.getTarget(userData)) {
+      const userLevel = this.addLevel(userData);
+      this.sendLevelUpMessage(client, guild, channels.ranking, id, name, userLevel);
     }
-    // save in the database
+
     return database.set(guild.id, { members });
+  }
 
-    function getTarget(user) {
-      return user.level * 10 + 100;
-    }
+  getTarget(user) {
+    return user.level * 10 + 100;
+  }
 
-    function addExperience(user) {
-      return (user.experience += 10);
-    }
+  addExperience(user) {
+    const EXPERIENCE_GAIN = 10;
+    user.experience += EXPERIENCE_GAIN;
+    return user.experience;
+  }
 
-    function addLevel(user) {
-      user.level++;
-      user.experience = 0;
+  addLevel(user) {
+    user.level++;
+    user.experience = 0;
+    return user.level;
+  }
 
-      return user.level;
+  async sendLevelUpMessage(client, guild, channelId, userId, userName, userLevel) {
+    const embed = {
+      title: "Level Up",
+      color: client.config.color.int.primary,
+      description: `${userName} has reached level **${userLevel}**!`,
+    };
+
+    const channel = guild.channels.cache.get(channelId);
+    if (channel) {
+      const permissions = channel.permissionsFor(client.user.id);
+      if (permissions.has(["EmbedLinks", "SendMessages"])) {
+        await channel.send({ content: `<@${userId}>`, embeds: [embed] });
+      }
     }
   }
 }
