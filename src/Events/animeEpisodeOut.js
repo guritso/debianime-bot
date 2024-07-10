203cc5ab116a8147a220aa5c895760880589aa84
GuriZenit <gurizenit@gmail.com>
2024-07-10 20:39:22 +0000

    fix: add permission check in animeEpisodeOut event

--- src/Events/animeEpisodeOut.js
+++ src/Events/animeEpisodeOut.js
@@ -1,3 +1,5 @@
+import Permission from "../Utils/Permission.js";
+
 export default class {
   constructor() {
     this.once = false;
@@ -6,6 +8,10 @@ export default class {
   async execute({ client, anime }) {
     const { title, link, episode, image, pubDate } = anime;
     const { database, config } = client;
+
+    const permission = new Permission()
+      .setMemberId(client.user.id)
+      .setNeeded(["EmbedLinks", "SendMessages"]);
 
     const embed = {
       title: `${title.split("#")[0]}`,
@@ -20,9 +26,14 @@ export default class {
       url: link,
     };
 
-    client.guilds.cache.forEach((guild) => {
+    client.guilds.cache.forEach(async (guild) => {
       const channel = guild.channels.cache.get(database.cache.get(guild.id).channels.anime_channel);
-      if (channel) {
+
+      permission.setChannel(channel);
+
+      const missing = await permission.getMissing();
+
+      if (channel && !missing) {
         channel.send({ embeds: [embed] });
       }
     });
