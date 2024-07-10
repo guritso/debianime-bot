c81bb12d0aef6616119ac1dac44116407d8fd7b7
GuriZenit <gurizenit@gmail.com>
2024-07-10 04:17:05 +0000

    refactor: inline subcommand logic in channel manager

--- src/Commands/Slash/Manager/channel.js
+++ src/Commands/Slash/Manager/channel.js
@@ -28,10 +28,38 @@ export default class {
       permissions: ["ManageChannels", "ViewChannel"],
     };
   }
-  async execute(interaction, client) {
+  async execute(interaction) {
     const subCommand = interaction.options.getSubcommand();
-    const Command = await import(`./channel/${subCommand}.js`);
-    const command = new Command.default();
-    command.execute(interaction);
+
+    const subCommands = {
+      rename: async () => {
+        const { color } = interaction.client.config;
+        const giveChannel = interaction.options.getChannel("channel");
+        const newName = interaction.options.getString("new_name");
+        const channel = giveChannel || interaction.channel;
+        const oldName = channel.name;
+
+        if (!channel.manageable)
+          return interaction.reply({
+            content: `> I don't have permission in ${oldName}`,
+            ephemeral: true,
+          });
+
+        await channel.setName(newName);
+
+        const embed = {
+          title: "Channel Renamed",
+          color: color.int.primary,
+          description: `**OLD:** ${oldName}\n**NEW:** ${newName}`,
+        };
+
+        interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        });
+      },
+    };
+
+    subCommands[subCommand]();
   }
 }
