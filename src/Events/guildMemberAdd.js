be31a08b07233bef0a2458b7bf131b9c22fd0c0d
GuriZenit <gurizenit@gmail.com>
2024-07-19 00:08:45 +0000

    fix: add permission check in guildMemberAdd event

--- src/Events/guildMemberAdd.js
+++ src/Events/guildMemberAdd.js
@@ -5,8 +5,20 @@ export default class {
 
   async execute({ guild, user, client }) {
     const { database, config } = client;
-    const channel = guild.channels.cache.get(database.cache.get(guild.id)?.channels.welcome_channel);
+    const channel = guild.channels.cache.get(
+      database.cache.get(guild.id)?.channels.welcome_channel
+    );
 
+    if (!channel) return;
+
+    const permission = new Permission()
+      .setMemberId(client.user.id)
+      .setChannel(channel)
+      .setNeeded(["EmbedLinks", "SendMessages"]);
+
+    const missing = await permission.getMissing();
+
+    if (missing) return;
 
     const embed = {
       color: config.color.int.primary,
